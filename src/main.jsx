411520ece2ca6d256fa5a1b113a31f9d9b3e8bc1
Rodney Hawkins <1850318+rodvdka@users.noreply.github.com>
2021-12-13 12:09:46 +0200

    Recent changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import GiftVoucher from './GiftVoucher'
+import PaymentRequest from './PaymentRequest'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <GiftVoucher />
-  </React.StrictMode>,
-  document.getElementById('swansong-loader')
-)
+var isVoucher = document.location.toString().indexOf("/our-counsellors") === -1;
+
+var loadPaymentCallback = (event) => {
+    console.log("React ready to roll with event", event);
+    ReactDOM.render(
+        <React.StrictMode>
+            {isVoucher ? <PaymentRequest transaction_type={"gift-card"} /> : <PaymentRequest transaction_type={"session"} />}
+        </React.StrictMode>,
+        document.getElementById('swansong-loader')
+      )
+}
+
+window.loadPaymentCallback = loadPaymentCallback;
+
+console.log("isVoucher", isVoucher);
+
+if (isVoucher === true || window.location.hash  === "#redeem-voucher") {
+    window.loadPaymentCallback(null);
+}
+
